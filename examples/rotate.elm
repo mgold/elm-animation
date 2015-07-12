f874f6223ac0c524684b19f09ead6eb384e2a4c8
Max Goldstein <maxgoldstein1@gmail.com>
2015-07-12 10:35:43 -0400

    Move radially as well

--- examples/rotate.elm
+++ examples/rotate.elm
@@ -9,8 +9,8 @@ import Mouse
 
 import Animation exposing (..)
 
-type alias Model = { theta : Animation, clock : Time }
-model0 = Model (static 0) 0
+type alias Model = { theta : Animation, r : Animation, clock : Time }
+model0 = Model (static 0) (static 100) 0
 
 mouseLocation : Signal (Float, Float)
 mouseLocation =
@@ -27,24 +27,30 @@ actions = Signal.merge
 step : Action -> Model -> Model
 step act model = case act of
     Tick dt -> {model| clock <- model.clock + dt}
-    Click (r, theta) -> {model| theta <- retarget model.clock theta model.theta |> speed 0.002 |> normalizeAngle}
+    Click (dest_r, dest_theta) ->
+        let theta = retarget model.clock dest_theta model.theta |> speed 0.002 |> normalizeAngle
+            r = retarget model.clock (min armLength dest_r) model.r |> speed 0.4
+            dur = max (getDuration theta) (getDuration r)
+        in {model| theta <- theta |> duration dur, r <- r |> duration dur}
 
 normalizeAngle anim =
     let from = getFrom anim
         to = getTo anim
     in if | abs (from - to) < (degrees 180) -> anim
-          | to < from -> Animation.to (to + turns 1) anim
-          | to > from -> Animation.to (to - turns 1) anim
+          | to < from -> normalizeAngle <| Animation.to (to + turns 1) anim
+          | to > from -> normalizeAngle <| Animation.to (to - turns 1) anim
 
 model : Signal Model
 model = Signal.foldp step model0 actions
 
+armLength = 400
 render : (Int, Int) -> Model -> Element
-render (w, h) {theta, clock} =
+render (w, h) {theta, r, clock} =
     let angle = animate clock theta
+        radius = animate clock r
         base = Collage.circle 5 |> Collage.filled Color.charcoal
-        arm = Collage.rect 400 3 |> Collage.filled Color.charcoal |> Collage.moveX 200
-        circle = Collage.circle 20 |> Collage.filled Color.red |> Collage.moveX 300
+        arm = Collage.rect armLength 3 |> Collage.filled Color.charcoal |> Collage.moveX (armLength / 2)
+        circle = Collage.circle 20 |> Collage.filled Color.red |> Collage.moveX radius
         group = Collage.group [base, arm, circle] |> Collage.rotate angle
     in Collage.collage w h [group]
 
