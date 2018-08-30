75430fd5c866e3204bfed7b9ad31c778b289600a
Max Goldstein <maxgoldstein1@gmail.com>
2018-08-29 22:57:14 -0700

    Update documentation and add some proper tests

--- src/Animation.elm
+++ src/Animation.elm
@@ -30,49 +30,7 @@ module Animation
         , undo
         )
 
-{-| A library for animating between two `Float` values. For example, animate a panel's width from 100px to 300px over 2
-seconds, or make a button spin and grow on hover. Everything is a pure function (no signals or tasks), so you can use it
-easily within your architecture. You can also inspect animations to determine if they are still running and for how
-long, and even smoothly retarget a different destination midflight.
-
-The library encapsulates a 3-stage animation pipeline:
-
-  - **Timekeeping:** Creating and running an animation requires the current time, which is best obtained by summing
-    [`Browser.Events.onAnimationFrameDelta`](https://package.elm-lang.org/packages/elm/browser/latest/Browser-Events#onAnimationFrameDelta).
-    You can also specify the duration of animation, and delay it prior to starting.
-
-  - **Easing:** An easing function makes an animation come alive with acceleration or even elasticity. You can find all
-    kinds of crazy easing functions in [this library](http://package.elm-lang.org/packages/elm-community/easing-functions/latest/Ease).
-
-  - **Interpolation:** It wouldn't be very useful is all animations went from 0 to 1 (the default), would it? You can
-    specify values to animate `from` and `to`. Furthermore, you can set the average speed (distance between these two
-    values per milisecond) instead of a duration.
-
-Once you have your value at the current time, you can render it to any frontend you choose: Collage, Element, Html,
-[Turtles](http://package.elm-lang.org/packages/mgold/elm-turtle-graphics/latest)...
-
-
-## Basic Usage
-
-`animation` creates an animation starting at the given time (usually the current time). `animate` takes the current time
-and an animation, and produces the current value. Animations go through three phases (not related to the three stages of
-rendering): they are scheduled, they run, and then they are done.
-
-    import Animation exposing (..)
-    import Time
-
-    myAnim = animation (Time.millisToPosix 0)
-      |> from 100
-      |> to 300
-      |> duration 4
-      |> delay 1
-
-    List.map (\t -> animate (Time.millisToPosix t) myAnim) (List.range 0 6)
-    -- [100, 100, 129.29, 200, 270.71, 300, 300]
-
-Notice that the value remains constant during the delay and after the animation is done. You can also use `static` to
-create animations of constant value. By using these two degenerate cases, you ought to be able to keep animations in
-your model without worrying about when they aren't actually animating.
+{-| Animate between two `Float` values. For examples and general usage, see the README.
 
 
 # Types
@@ -398,20 +356,28 @@ to x (A a) =
 are still scheduled, and is not bounded for animations that are already done.
 -}
 timeElapsed : Clock -> Animation -> TimeDelta
-timeElapsed clock (A { start, delay_ }) =
-    clock - (start + delay_) |> max 0
+timeElapsed clock ((A { start, delay_ }) as u) =
+    if isStatic u then
+        0
+
+    else
+        clock - (start + delay_) |> max 0
 
 
-{-| Get the time that the animation has yet to play (or be delayed) before becoming done. Will be zero for animations
+{-| Get the time until the animation is done. This time may be spent animating or be part of the delay. Will be zero for animations
 that are already done.
 -}
 timeRemaining : Clock -> Animation -> TimeDelta
-timeRemaining clock (A { start, delay_, dos, from_, to_ }) =
-    let
-        duration_ =
-            dur dos from_ to_
-    in
-    start + delay_ + duration_ - clock |> max 0
+timeRemaining clock ((A { start, delay_, dos, from_, to_ }) as u) =
+    if isStatic u then
+        0
+
+    else
+        let
+            duration_ =
+                dur dos from_ to_
+        in
+        start + delay_ + duration_ - clock |> max 0
 
 
 {-| Get the _current_ velocity of the animation, aproximated by looking 10ms forwards and backwards (the central
